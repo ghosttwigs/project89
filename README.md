07bdef37e6b7c5fc4da2ed2228a39b42acb84d16
ghosttwigs <145803068+ghosttwigs@users.noreply.github.com>
2023-11-01 14:14:54 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project89
+# spectagram-stage-9
+project solution c89
---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARMEWWIoKEA8thbL5i4FN9ozeVHwLHWq0",
+    authDomain: "spectigram.firebaseapp.com",
+    projectId: "spectigram",
+    storageBucket: "spectigram.appspot.com",
+    messagingSenderId: "393766821607",
+    appId: "1:393766821607:web:0beb5f0d27d04b25038c42"
+  };
